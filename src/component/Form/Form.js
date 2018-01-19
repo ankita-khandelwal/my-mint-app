4fcc21710614df1ed1fb6acdc8bbd673fb2ce460
deepmistry <pinkskhandelwal@gmail.com>
2018-01-19 05:06:24 -0500

    Added type checking to Form

--- src/component/Form/Form.js
+++ src/component/Form/Form.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 export default class Form extends Component {
 
@@ -17,3 +18,7 @@ export default class Form extends Component {
     )
   }
 }
+
+Form.propTypes = {
+  createFormContents: PropTypes.func.isRequired,
+};
